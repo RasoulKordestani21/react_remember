a6787f77a3f8233322512e620ac5ffd19a43642f
rasoul <rasoulkordestani12@gmail.com>
2021-05-29 17:19:14 +0430

    5_3 or 7  add and remove with unmount concept

--- react_with_node/src/pages/TodoHome/TodoHome.page.js
+++ react_with_node/src/pages/TodoHome/TodoHome.page.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import Form from './Components/Form/Form.component'
 export class TodoHomePage extends Component {
+    state = {
+        flag: true
+    }
+    componentDidMount() {
+        console.log("form mounted");
+    }
+    componentWillUnmount() {
+        console.log("form unmounted")
+    }
     render() {
         return (
             <div>
                 <header>header</header>
-                <Form />
+                {this.state.flag && <Form />}
+                <button onClick={() => { this.setState({ flag: true }) }}>form adder</button>
+                <button onClick={() => { this.setState({ flag: false }) }}>form remover</button>
             </div>
         )
     }
