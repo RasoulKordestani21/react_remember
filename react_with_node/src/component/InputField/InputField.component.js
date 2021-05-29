974332429f3e27a93e10d0ad4faf214a9d05ffef
rasoul <rasoulkordestani12@gmail.com>
2021-05-29 16:49:28 +0430

    6_ make two seperate way on graph

---
+++ react_with_node/src/component/InputField/InputField.component.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class InputField extends Component {
+    state = {
+        value: ""
+    }
+
+    
+    changeHandler = async ({ target }) => {
+
+        // /*method_1 wighout await*//*target is destructure !!!*/this.setState({ value: target.value });
+        await /*method_2 with await*/this.setState({ value: target.value })
+        this.props.onChangeInput(this.state.value);
+    }
+
+    componentDidMount() {
+        console.log("mounted")
+    }
+    render() {
+        console.log("didmou")
+
+        /*destructure also applied to here*/
+        const { value } = this.state;
+        const { title = "set a title please" } = this.props;
+        return (
+            <div>
+                <label>{title}</label>
+                <input type="text" value={value} onChange={this.changeHandler} />
+            </div>
+        )
+    }
+}
+
+export { InputField }
