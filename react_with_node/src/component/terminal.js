13a7f9540e823d871366134de1a5e5f42946375d
rasoul <rasoulkordestani12@gmail.com>
2021-06-01 18:36:00 +0430

    5_7 MouseTracker inherit tracking properity from Mouse

--- react_with_node/src/component/terminal.js
+++ react_with_node/src/component/terminal.js
@@ -1,3 +1,4 @@
 export { InputField } from './InputField/InputField.component';
 export { Button } from './Button/Button.component';
 export { MouseTracker } from './MouseTracker/MouseTracker.component'
+export { Mouse } from './MouseTracker/Mouse.component'
